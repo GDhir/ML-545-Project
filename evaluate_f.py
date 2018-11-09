56fa3d9a312bdbf8dcd7dcc214bc08ce44a5d52f
jose <jmfaunde@umich.edu>
2018-11-09 13:46:23 -0500

    update 10/9

--- evaluate_f.py
+++ evaluate_f.py
@@ -5,9 +5,12 @@ import time
 from functions import read_data, neural_net_model_1, denormalize
 
 X_train, y_train_n, X_test, y_test_n,  y_train, y_test= read_data()
-Cd_test_ok = y_test[:,2]
-xs = tf.placeholder("float")
-ys = tf.placeholder("float")
+Cd_train = np.reshape(y_train_n[:,2],[-1,1])
+Cd_test = np.reshape(y_test_n[:,2],[-1,1])
+Cd_test_ok = np.reshape(y_test[:,2],[-1,1])
+Cd_train_ok = np.reshape(y_train[:,2],[-1,1])
+xs = x = tf.placeholder(tf.float32, shape=[None, X_train.shape[1]],name="xs") 
+ys =  tf.placeholder(tf.float32, shape=[None, 1],name="ys")
 output = neural_net_model_1(xs,16)
 cost = tf.reduce_mean(tf.square(output-ys))
 saver = tf.train.Saver()
@@ -15,17 +18,25 @@ with tf.Session() as session:
     #Load NN
     saver.restore(session, './data/NN_Cd_1.ckpt')
     # Evalute test data
-    pred = session.run(output,feed_dict={xs:X_test})
-    Mse1 = np.mean((pred-y_test_n[:,2])**2)
-    Mse2 = session.run(cost, feed_dict={xs:X_test,ys:y_test_n[:,2]})
+    pred_n = session.run(output,feed_dict={xs:X_test})
+    Mse1 = np.mean((pred_n-y_test_n[:,2])**2)
+    Mse2 = session.run(cost, feed_dict={xs:X_test,ys:Cd_test})
     print(Mse1,Mse2)
-    pred = denormalize(Cd_test_ok,pred)
+    pred = denormalize(Cd_train_ok,pred_n)
     
     Mse = np.mean((pred-Cd_test_ok)**2)
     print(Mse)
     #Plot results
-    plt.plot(range(Cd_test_ok.shape[0]),Cd_test_ok,label="Original Data")
+    plt.figure()
     plt.plot(range(Cd_test_ok.shape[0]),pred,label="Predicted Data")
+    plt.plot(range(Cd_test_ok.shape[0]),Cd_test_ok,label="Original Data")
+    plt.legend(loc='best')
+    plt.ylabel(r'$C_d$')
+    plt.xlabel('Test samples')
+    plt.show()
+    plt.figure()
+    plt.plot(range(Cd_test.shape[0]),pred_n,label="Predicted Data")
+    plt.plot(range(Cd_test.shape[0]),Cd_test,label="Original Data")
     plt.legend(loc='best')
     plt.ylabel(r'$C_d$')
     plt.xlabel('Test samples')
--- train_code.py
+++ train_code.py
@@ -4,12 +4,18 @@ import matplotlib.pyplot as plt
 import time
 from functions import read_data, neural_net_model_1, denormalize
 
+def Next_Batch(X,y,batch,batch_size):
+    #i = np.random.randint(0,len(X)-batch_size)
+    batch_x = X[batch:batch+batch_size,:]
+    batch_y = y[batch:batch+batch_size]
+    return batch_x, batch_y
+
 #Parameters
 name = "Cd_1"
 alphaa = 'variable'
 alpha_i = 0.002
 n_neur = 10
-optimizer = "gradient"
+optimizer = "Adam"
 n_layers = 2 #change functions file
 
 f = open("./data/"+name,"w+")
@@ -20,12 +26,13 @@ f.write("%5.8f \n" %n_neur)
 f.write("%5.8f \n" %n_layers)
 
 X_train, y_train_n, X_test, y_test_n, y_train, y_test = read_data() #Read de data
-Cd_train = y_train_n[:,2]
-Cd_test = y_test_n[:,2]
-Cd_test_ok = y_test[:,2]
+Cd_train = np.reshape(y_train_n[:,2],[-1,1])
+Cd_test = np.reshape(y_test_n[:,2],[-1,1])
+Cd_test_ok = np.reshape(y_test[:,2],[-1,1])
+Cd_train_ok = np.reshape(y_train[:,2],[-1,1])
 n_x = 16
-xs = tf.placeholder("float")
-ys = tf.placeholder("float")
+xs = x = tf.placeholder(tf.float32, shape=[None, X_train.shape[1]],name="xs") 
+ys =  tf.placeholder(tf.float32, shape=[None, 1],name="ys")
 alpha = tf.placeholder("float")
 output = neural_net_model_1(xs,n_x, n_neur)
 cost = tf.reduce_mean(tf.square(output-ys)) # our mean squared error cost function
@@ -39,12 +46,17 @@ c_t =[]
 saver = tf.train.Saver()
 start = time.time()
 
+
+batch_size = int(X_train.shape[0]*0.05)
+batches = int(X_train.shape[0]/batch_size)
 with tf.Session() as sess:
     # Initiate session and initialize all vaiables
     sess.run(tf.global_variables_initializer())
-    for i in range(25):
-        for j in range(X_train.shape[0]):
-            sess.run(train,feed_dict= {xs:X_train[j,:].reshape(1,n_x), ys:Cd_train[j], alpha: alpha_i})
+    for i in range(100):
+        for j in range(batches):
+            batch = (i+1)*batch_size    
+            batch_x,batch_y=Next_Batch(X_train,Cd_train,batch,batch_size)
+            sess.run(train,feed_dict= {xs:batch_x, ys:batch_y, alpha: alpha_i})
             # Run train with each sample
         c_t.append(sess.run(cost, feed_dict={xs:X_train,ys:Cd_train}))
         print('Epoch :',i,'Cost :',c_t[i])
