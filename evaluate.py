10585d1e266130c68a43fde5b164f2a6e70fa8b9
jose <jmfaunde@umich.edu>
2018-11-10 22:08:27 -0500

    class NN created

--- evaluate.py
+++ evaluate.py
@@ -23,18 +23,6 @@ with tf.Session() as session:
     Mse1 = np.mean((pred_n-y_test_n[:,2])**2)
     Mse2 = session.run(cost, feed_dict={xs:X_test,ys:Cd_test})
     print(Mse1,Mse2)
-    # pred = denormalize(Cd_train_ok,pred_n)
-    
-    # Mse = np.mean((pred-Cd_test_ok)**2)
-    # print(Mse)
-    #Plot results
-    # plt.figure()
-    # plt.plot(range(Cd_test_ok.shape[0]),pred,label="Predicted Data")
-    # plt.plot(range(Cd_test_ok.shape[0]),Cd_test_ok,label="Original Data")
-    # plt.legend(loc='best')
-    # plt.ylabel(r'$C_d$')
-    # plt.xlabel('Test samples')
-    # plt.show()
     plt.figure()
     plt.plot(range(Cd_test.shape[0]),pred_n,label="Predicted Data")
     plt.plot(range(Cd_test.shape[0]),Cd_test,label="Original Data")
@@ -42,4 +30,3 @@ with tf.Session() as session:
     plt.ylabel(r'$C_d$')
     plt.xlabel('Test samples')
     plt.show()
-    
