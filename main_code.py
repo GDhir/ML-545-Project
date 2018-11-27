7a0fd13b888c600485b35dc27393133919a929b0
jose <jmfaunde@umich.edu>
2018-11-26 21:17:54 -0500

    Update 11/26, evaluate airfoils

--- main_code.py
+++ main_code.py
@@ -8,8 +8,8 @@ def read_data():
     xy0, xy1, xy2 represent files corresponding to three outputs (CL, CM and CD)
     xy0:
     x: (x_geo (7 thickness, 7 cmaber) Ma, alpha),
-    y: (CL), CM, CD,
-    pCL/px, pCM/px, pCD/px
+    y: (CL), CD, CM,
+    pCL/px, pCM/px, pCD/px (??? not sure about the order)
     """
     np.random.seed(0)
     xy0 = np.loadtxt('M0CFDdata.txt')
@@ -25,11 +25,32 @@ def read_data():
     y_train_ok = y[:N_train,:]
     y_test_ok = y[N_train:,:]
     scaler = MinMaxScaler()
+    x_train = x[:N_train,:]
+    np.savetxt('x_train.txt',x_train)
+    np.savetxt('y_train.txt', y_train_ok)
+    x_train = scaler.fit_transform(x_train) #normalize the X_train
+    x_test = scaler.transform(x[N_train:,:]) #normalize the X_test
+    y_train_n = scaler.fit_transform(y[:N_train,:]) #normalized data
+    y_test_n = scaler.transform(y[N_train:,:]) #normalized data
+    return x_train, y_train_n, x_test, y_test_n, y_train_ok, y_test_ok
+
+def read_newdata():
+    np.random.seed(0)
+    xy = np.loadtxt('new_data.txt')
+    np.random.shuffle(xy)
+    dim = 281+2 #281 points + Mach number + angle of attack 
+    x = xy[:, :dim]
+    y = xy[:, dim:dim+3]
+    N_train = np.int(len(y[:,0])*0.8) #Number of train data
+    y_train_ok = y[:N_train,:]
+    y_test_ok = y[N_train:,:]
+    scaler = MinMaxScaler()
     x_train = scaler.fit_transform(x[:N_train,:]) #normalize the X_train
     x_test = scaler.transform(x[N_train:,:]) #normalize the X_test
     y_train_n = scaler.fit_transform(y[:N_train,:]) #normalized data
     y_test_n = scaler.transform(y[N_train:,:]) #normalized data
     return x_train, y_train_n, x_test, y_test_n, y_train_ok, y_test_ok
+
 
 X_train, y_train, X_test, y_test, y_train_ok, y_test_ok = read_data() #Read de data
 
@@ -38,25 +59,27 @@ Cl_test = np.reshape(y_test[:,0],[-1,1])
 Cl_ok = np.reshape(y_train_ok[:,0],[-1,1])
 Cl_test_ok = np.reshape(y_test_ok[:,0],[-1,1])
 
-Cm_train = np.reshape(y_train[:,1],[-1,1])
-Cm_test = np.reshape(y_test[:,1],[-1,1])
-Cm_ok = np.reshape(y_train_ok[:,1],[-1,1])
-Cm_test_ok = np.reshape(y_test_ok[:,1],[-1,1])
+Cm_train = np.reshape(y_train[:,2],[-1,1])
+Cm_test = np.reshape(y_test[:,2],[-1,1])
+Cm_ok = np.reshape(y_train_ok[:,2],[-1,1])
+Cm_test_ok = np.reshape(y_test_ok[:,2],[-1,1])
 
-Cd_train = np.reshape(y_train[:,2],[-1,1])
-Cd_test = np.reshape(y_test[:,2],[-1,1])
-Cd_ok = np.reshape(y_train_ok[:,2],[-1,1])
-Cd_test_ok = np.reshape(y_test_ok[:,2],[-1,1])
+Cd_train = np.reshape(y_train[:,1],[-1,1])
+Cd_test = np.reshape(y_test[:,1],[-1,1])
+Cd_ok = np.reshape(y_train_ok[:,1],[-1,1])
+Cd_test_ok = np.reshape(y_test_ok[:,1],[-1,1])
 
 #%%
+
 alpha = .001
 n_neur = 60
-n_layers = 3
-epc = 5000
+n_layers = 2
+x_dim  = 16#281+2
+epc = 6000
 save = True
 
 #create NN for Cd
-model_Cd = NeuralAirfoil(N_hlayers=n_layers, n_neur=n_neur, learning_rate=alpha, num_epochs=epc)
+model_Cd = NeuralAirfoil(x_dim = x_dim, N_hlayers=n_layers, n_neur=n_neur, learning_rate=alpha, num_epochs=epc)
 #Train the NN for Cd
 model_Cd.train_NN(X_train, Cd_train, X_test, Cd_test, Cd_ok, Cd_test_ok)
 #generate the plots for Cd
@@ -65,8 +88,7 @@ print('Relative error test %',model_Cd.R_error_test[-1])
 print('Relative error train %',model_Cd.R_error_train[-1])
 
 
-
-model_Cm = NeuralAirfoil(N_hlayers=n_layers, n_neur=n_neur, learning_rate=alpha, num_epochs=epc)
+model_Cm = NeuralAirfoil(x_dim = x_dim, N_hlayers=n_layers, n_neur=n_neur, learning_rate=alpha, num_epochs=epc)
 model_Cm.train_NN(X_train, Cm_train, X_test, Cm_test, Cm_ok, Cm_test_ok)
 model_Cm.generate_plot('Cm', show=True, save=save)
 print('Relative error test %',model_Cm.R_error_test[-1])
@@ -74,7 +96,7 @@ print('Relative error train %',model_Cm.R_error_train[-1])
 
 alpha = .01
 
-model_Cl = NeuralAirfoil(N_hlayers=n_layers, n_neur=n_neur, learning_rate=alpha, num_epochs=epc)
+model_Cl = NeuralAirfoil(x_dim = x_dim, N_hlayers=n_layers, n_neur=n_neur, learning_rate=alpha, num_epochs=epc)
 model_Cl.train_NN(X_train, Cl_train, X_test, Cl_test, Cl_ok, Cl_test_ok)
 model_Cl.generate_plot('Cl', show=True, save=save)
 print('Relative error test %',model_Cl.R_error_test[-1])
---
+++ modes_to_points.py
@@ -0,0 +1,29 @@
+#%%
+import numpy as np
+import matplotlib.pyplot as plt
+
+xy0 = np.loadtxt('M0CFDdata.txt')
+xy1 = np.loadtxt('M1CFDdata.txt')
+xy2 = np.loadtxt('M2CFDdata.txt')
+xy = np.concatenate((np.concatenate((xy0, xy1)), xy2))
+
+basedata = np.loadtxt('basis.txt')
+x = basedata[0,:].copy()
+U = basedata[1:,:].copy() 
+N_data = xy.shape[0]
+N_points = U.shape[1] 
+new_data = np.zeros((N_data, N_points + 5)) 
+
+for i in range(N_data):
+    new_data[i,0:N_points] = np.dot(xy[i,0:14],U)
+new_data[:,N_points:N_points+5] = xy[:,14:19]
+np.savetxt('new_data.txt',new_data)
+np.savetxt('x_geom.txt',x)
+#%%
+new = np.loadtxt('new_data.txt')
+x = np.loadtxt('x_geom.txt')
+#%%
+i = 10000
+plt.plot(x, new[i,0:281])
+plt.axis('equal')
+print(new[i,281:])
